e3e9e00a9fe41e5f079483233227ec751a1f8687
Hyunil Suk <74586346+sukhyunil@users.noreply.github.com>
2021-01-06 17:58:47 +0900

    Add files via upload

---
+++ Java1/programing/programing/program.java
@@ -0,0 +1,13 @@
+package programing;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);		
+
+	}
+
+}
